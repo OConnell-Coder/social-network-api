cc9fcf1bfc21765eb587e73ac356c7feedb18dc8
Codename O'Connell <oconnellcoder@gmail.com>
2023-04-08 11:17:22 -0400

    fixed error

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-const {getUsers, createUser} = require('../../controllers');
+const {getUsers, createUser} = require('../../controllers/userController');
 
 router.route('/')
     .post(createUser)
-    //server crashing, getting an error here:
-    //"Route.post() requires a callback function but got a [object Undefined]"
+
     .get(getUsers);
 
 module.exports = router;
